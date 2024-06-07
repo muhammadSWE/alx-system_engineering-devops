499584255ba07cc9fcefbc352068d2e7c3ac44ab
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2024-06-07 22:11:06 +0300

    fix

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Function to query a list of all hot posts on a given Reddit subreddit."""
+"""A recursive function that queries the Reddit
+API and returns a list containing the titles of
+all hot articles for a given subreddit."""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
+    """returns a list containing the titles
+    of all hot articles for a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
